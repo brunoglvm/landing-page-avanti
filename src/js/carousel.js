9c1b3c257bc809d7e4e0110d4c411b3c4d6fe6f8
Bruno Galvão <bgalvaomiranda302@gmail.com>
2025-04-22 09:31:17 -0300

    style: update carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,35 +1,27 @@
-import { products } from "../data/products.js";
-
 const carouselInner = document.getElementById("carouselInner");
 
-const baseProduct = products[0];
 const totalItems = 10;
 const itemsPerSlide = 5;
-const productList = Array.from({ length: totalItems }, () => baseProduct);
 
-const slidesCount = Math.ceil(productList.length / itemsPerSlide);
+const slidesCount = Math.ceil(totalItems / itemsPerSlide);
 
 for (let i = 0; i < slidesCount; i++) {
   const isActive = i === 0 ? "active" : "";
-  const slideProducts = productList.slice(
-    i * itemsPerSlide,
-    (i + 1) * itemsPerSlide
-  );
 
-  const cardsHTML = slideProducts
+  const cardsHTML = Array.from({ length: itemsPerSlide })
     .map(
-      (product) => `
+      () => `
         <div class="carousel__col">
           <div class="product-card">
             <div class="product-card__image-wrapper">
-              <span class="product-card__tag">${product.tag}</span>
-              <img src="${product.image}" class="product-card__image" alt="${product.title}" />
+              <span class="product-card__tag">Novo</span>
+              <img src="src/assets/images/product.webp" class="product-card__image" alt="Lorem ipsum dolor sit amet consectetuer adipiscing elit" />
             </div>
             <div class="product-card__body">
-              <h5 class="product-card__title">${product.title}</h5>
-              <p class="product-card__original-price">${product.originalPrice}</p>
-              <p class="product-card__discounted-price">${product.discountedPrice} <small class="product-card__discount">${product.discount}</small></p>
-              <p class="product-card__installments">${product.installments}</p>
+              <h5 class="product-card__title">Lorem ipsum dolor sit amet consectetuer adipiscing elit</h5>
+              <p class="product-card__original-price">R$ 100,00</p>
+              <p class="product-card__discounted-price">R$79,90 <small class="product-card__discount">10% off</small></p>
+              <p class="product-card__installments">Ou em até <span class="product-card__installments-highlight">10x de R$ 7,90</span></p>
               <button class="product-card__button">Comprar</button>
             </div>
           </div>

db5beb2cb72bad77cef3729ceaee7266de16dec5
Bruno Galvão <bgalvaomiranda302@gmail.com>
2025-04-22 14:19:53 -0300

    style(carousel): update buttons

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,6 +1,6 @@
 const carouselInner = document.getElementById("carouselInner");
 
-const totalItems = 10;
+const totalItems = 15;
 const itemsPerSlide = 5;
 
 const slidesCount = Math.ceil(totalItems / itemsPerSlide);
@@ -11,29 +11,34 @@ for (let i = 0; i < slidesCount; i++) {
   const cardsHTML = Array.from({ length: itemsPerSlide })
     .map(
       () => `
-        <div class="carousel__col">
-          <div class="product-card">
-            <div class="product-card__image-wrapper">
-              <span class="product-card__tag">Novo</span>
-              <img src="src/assets/images/product.webp" class="product-card__image" alt="Lorem ipsum dolor sit amet consectetuer adipiscing elit" />
+      <div class="carousel__col">
+        <div class="product-card">
+          <div class="product-card__image-wrapper">
+            <span class="product-card__tag">Novo</span>
+            <img src="src/assets/images/product.webp" class="product-card__image" alt="Lorem ipsum dolor sit amet consectetuer adipiscing elit" />
+          </div>
+          <div class="product-card__body">
+            <h5 class="product-card__title">Lorem ipsum dolor sit amet consectetuer adipiscing elit</h5>
+            <div class="product-card__price">
+              <div class="product-card__price-details">
+                <p class="product-card__original-price">R$ 100,00</p>
+                <p class="product-card__discounted-price">R$79,90</p>
+              </div>
+              <p class="product-card__discount">10% off</p>
             </div>
-            <div class="product-card__body">
-              <h5 class="product-card__title">Lorem ipsum dolor sit amet consectetuer adipiscing elit</h5>
-              <p class="product-card__original-price">R$ 100,00</p>
-              <p class="product-card__discounted-price">R$79,90 <small class="product-card__discount">10% off</small></p>
-              <p class="product-card__installments">Ou em até <span class="product-card__installments-highlight">10x de R$ 7,90</span></p>
-              <button class="product-card__button">Comprar</button>
-            </div>
+            <p class="product-card__installments">Ou em até <span class="product-card__installments-highlight">10x de R$ 7,90</span></p>
+            <button class="product-card__button">Comprar</button>
           </div>
         </div>
-      `
+      </div>
+    `
     )
     .join("");
 
   const slideHTML = `
     <div class="carousel-item ${isActive}">
       <div class="carousel__row">
-        ${cardsHTML}
+      ${cardsHTML}
       </div>
     </div>
   `;
